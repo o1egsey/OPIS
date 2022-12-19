5bc081f6e107d72f4025d615411fd7cb22168557
o1egsey <oleg.anischenko00@gmail.com>
2022-12-19 15:40:17 +0200

    added 2 classes

---
+++ IEIT/IEIT.py
@@ -0,0 +1,21 @@
+"""
+    this module will provide functionality for calculate_distance, get neighbors, optimize radius and delta.
+"""
+
+
+class IEIT:
+
+    def __init__(self, *rec_classes):
+        self.rec_classes = rec_classes
+
+    def calculate_distance(self):
+        pass
+
+    def get_neighbor(self):
+        pass
+
+    def optimize_radius(self):
+        pass
+
+    def optimize_delta(self):
+        pass
--- IEIT/learning.py
+++ IEIT/learning.py
@@ -10,6 +10,11 @@ class Learning:
         self.filename = filename
         self.delta = 20
         self.matrix = []
+        self.binary_matrix = []
+        self.avg_vector = []
+        self.higher_limit = []
+        self.lower_limit = []
+        self.etalon_vector = []
 
     def image_to_matrix(self, size=50):
         """Convert image to matrix with values of pixel RGBs"""
@@ -20,29 +25,61 @@ class Learning:
         return [alist[i * length // size: (i + 1) * length // size] for i in range(size)]
 
     def get_avg_vector(self):
+        self.avg_vector = list(numpy.mean(self.matrix, axis=0))
         return list(numpy.mean(self.matrix, axis=0))
 
     def get_limits(self):
-        pass
+        high_limit = []
+        low_limit = []
+        index = 0
+        for pixel in self.avg_vector:
+            high_limit.append(self.avg_vector[index] + self.delta)
+            low_limit.append(self.avg_vector[index] - self.delta)
+            index += 1
+
+        self.higher_limit = high_limit
+        self.lower_limit = low_limit
+        return high_limit, low_limit
 
     def matrix_to_binary(self):
-        pass
+        binary_matrix = []
+
+        for i in self.matrix:
+            row = []
+            for j in i:
+                if self.lower_limit[0] <= j <= self.higher_limit[0]:
+                    row.append(1)
+                else:
+                    row.append(0)
+            binary_matrix.append(row)
+
+        self.binary_matrix = binary_matrix
+        return binary_matrix
 
     def get_etalon_vector(self):
-        pass
+        counter_0 = [0] * 50
+        counter_1 = [0] * 50
+        etalon = []
 
-    def calculate_distance(self):
-        pass
+        for row in self.binary_matrix:
+            index = 0
+            for num in row:
+                if num == 0:
+                    counter_0[index] += 1
+                else:
+                    counter_1[index] += 1
+                index += 1
+        for i, j in zip(counter_0, counter_1):
+            if i > j:
+                etalon.append(0)
+            else:
+                etalon.append(1)
 
-    def get_neighbor(self):
-        pass
+        self.etalon_vector = etalon
+        return etalon
 
-    def get_vector(self):
-        pass
+    def get_vector(self, row_id):
+        """This function returns one row from binary matrix"""
+        return self.binary_matrix[row_id]
 
-    def optimize_radius(self):
-        pass
-
-    def optimize_delta(self):
-        pass
 
